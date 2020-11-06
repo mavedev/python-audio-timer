458d5fb4f55b0e5adc860878d1304c89c1c925b5
Cirill Usatchoff <usatchoff@gmail.com>
2020-11-06 18:51:50 +0200

    Basic stuff provided

---
+++ app/constants.py
@@ -0,0 +1,2 @@
+class Constants:
+    DEFAULT_TASK_PRIORITY = 1
---
+++ app/schedule_actor.py
@@ -0,0 +1,23 @@
+from collections.abc import Callable
+
+import sched
+import time
+
+from singletons import Singleton
+
+from .constants import Constants
+
+
+class ScheduleActor(metaclass=Singleton):
+    def __init__(self, *, duration: int) -> None:
+        self._duration = duration
+        self._scheduler = sched.scheduler(time.time, time.sleep)
+
+    def run_task(self, task: Callable, *args: tuple, **kwargs: dict) -> None:
+        task(*args, **kwargs)
+        self._scheduler.enter(
+            self._duration,
+            Constants.DEFAULT_TASK_PRIORITY,
+            self.run_task,
+            (lambda: task(*args, **kwargs), )
+        )
