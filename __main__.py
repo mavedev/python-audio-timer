b5c175462ceea27480516040a620c3aea777d4c1
Cirill Usatchoff <usatchoff@gmail.com>
2020-11-06 19:47:09 +0200

    Argument reading implemented

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+
 from playsound import playsound
 
 from app.schedule_actor import ScheduleActor
@@ -8,13 +10,20 @@ from app.invariants import Constants
 
 
 def main() -> None:
+    if len(sys.argv) != 2:
+        print('Invalid argument amount, must be 1')
+        return
+
     try:
-        ScheduleActor(duration=10).run_task(
+        seconds_provided = int(sys.argv[1])
+        ScheduleActor(duration=seconds_provided).run_task(
             playsound,
             Constants.DEFAULT_FILE_PATH
         )
     except KeyboardInterrupt:
         print('Program finished by user.')
+    except ValueError:
+        print('Invalid argument. Duration must be number of seconds.')
 
 
 if __name__ == '__main__':
